e20e4158699865cec3133aa212c3e7be287263df
huangxin11 <huangxin11@58ganji-corp.com>
2018-11-02 11:44:02 +0800

    优化

--- src/mixin/form.js
+++ src/mixin/form.js
@@ -7,7 +7,7 @@ export default class Form extends wepy.mixin {
     data = {};
     requestFormidCollect(formId) {
         if (formId) {
-            get('http://bossapi.58.com/smallapp/formid/set', {
+            get('https://bossapi.58.com/smallapp/formid/set', {
                 data: {
                     code: fuwu.globalData.token || 'cde2a69612ab49101d7009815566f1e5', // 当前用户 ID
                     formid: formId,
